253bdb7af42acb75f0519576a77fcde5f0d61f8f
John Swan <johneswan3@protonmail.com>
2022-05-09 04:02:17 -0600

    Changed Home Hero

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import Link from 'next/link'
 export default function Hero() {
   return (
-    <div className="relative">
+    <div className="relative -mt-[64px]">
       <Image
         layout="fill"
         placeholder="blur"
@@ -12,14 +13,34 @@ export default function Hero() {
         alt=""
         priority
       />
-      <div className="relative bg-slate-900 bg-opacity-75 p-5 backdrop-blur-sm md:p-20">
-        <div className="mx-auto px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-20">
-          <div className="flex flex-col items-center justify-between xl:flex-row">
-            <div className="w-full max-w-xl xl:w-7/12 xl:pr-16">
-              <h2 className="max-w-lg font-sans text-3xl font-semibold tracking-tight text-white sm:text-4xl sm:leading-none">
-                John Swan
-              </h2>
-            </div>
+      <div className=" flex h-screen items-center justify-center bg-slate-900 bg-opacity-75 text-right backdrop-blur-sm">
+        <div className="flex flex-col items-center space-y-6">
+          <div className="space-y-4 border-b border-cyan-600 pb-6">
+            <h1 className="max-w-lg font-sans text-4xl font-semibold tracking-tight text-white sm:text-5xl sm:leading-none md:text-6xl xl:text-7xl">
+              John Swan
+            </h1>
+            <p className="text-sm text-slate-200 md:text-base lg:text-lg">
+              Front End Web-Developer
+            </p>
+          </div>
+          <hr className="border-b border-white" />
+          <div className="flex space-x-4">
+            <Link href="/portfolio">
+              <a
+                aria-label="Go to projects"
+                className="focus:shadow-outline inline-flex h-12 items-center justify-center rounded bg-cyan-600 px-6 font-medium tracking-wide text-white shadow-md transition duration-200 hover:bg-slate-500 focus:outline-none"
+              >
+                Projects
+              </a>
+            </Link>
+            <Link href="/blog">
+              <a
+                aria-label="Go to blog"
+                className="focus:shadow-outline inline-flex h-12 items-center justify-center rounded px-6 font-medium tracking-wide text-white shadow-md transition duration-200 hover:bg-cyan-600 focus:outline-none"
+              >
+                My Blog
+              </a>
+            </Link>
           </div>
         </div>
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,8 +10,8 @@ export default function Navbar() {
       <nav className="bg-slate-900 shadow-md" id="nav">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
+            <div className="flex items-center justify-between">
+              <div className="flex-shrink-0\">
                 <Link href="/">
                   <a aria-label="Home">
                     <h1 className="text-2xl font-bold text-cyan-500">JS</h1>
